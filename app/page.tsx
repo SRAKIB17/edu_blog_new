9ad8841f4e57f9658084403a1218d918cfb05dd7
SRAKIB17 <rakibulssc5@gmail.com>
2023-05-08 16:39:32 +0600

    'gin

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image'
-export async function getData() {
+async function getData() {
   let res = await fetch('https://prog-learn.vercel.app/9b836a9c57a91ce7805cc6a0/cdn/e6df72-9b836a9c57a91ce7805cc6a0');
   return res.json();
 }
