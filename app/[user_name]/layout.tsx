093b07124f2db4e27a1856fa87de6518fc256085
SRAKIB17 <rakibulssc5@gmail.com>
2023-05-10 01:37:30 +0600

    'done'

--- app/[user_name]/layout.tsx
+++ app/[user_name]/layout.tsx
@@ -1,8 +1,10 @@
 import { Metadata } from "next"
 
 export async function generateMetadata({ params }: { params: { user_name: string } }): Promise<Metadata> {
+  const check_user_name = params?.user_name?.[0] == '~'
+
   return {
-    title: `${params?.user_name?.slice(1)}`
+    title: `${check_user_name ? params?.user_name?.slice(1) : "Could not find user"}`
   }
 }
 
